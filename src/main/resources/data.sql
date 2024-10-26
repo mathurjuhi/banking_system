150080e06a927bf6d5d6ebfa194e7498403b5d9e
msjava-proj <shreyalahariya@gmail.com>
2024-10-26 15:54:24 +0530

    entity & table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,10 @@
-
-INSERT INTO Account VALUES(1, 'Richa','Ranjan');
-INSERT INTO Account VALUES(2, 'Shriya','Lahariya');
-INSERT INTO Account VALUES(3, 'Juhi','Mathur');
-INSERT INTO Account VALUES(4, 'Shilpa','Shekhawat');
+INSERT INTO User_Account(account_id, first_name, last_name, user_id,pass,addres,email,mobile, balance) 
+VALUES(1 , 'Richa','Ranjan', 'richaR', 'csf12cdzxa', 'HAL Bangalore','richaranjan@yahoo.com', '8877664411', 8022000.90);
+INSERT INTO User_Account(account_id, first_name, last_name, user_id,pass,addres,email,mobile, balance) 
+VALUES(2, 'Shilpa', 'Shekhawat', 'shilpaS', '54frq1nj', 'Bommanahalli Mumbai', 'shilpashekhawat@rediffmail.com', '9911226644', 2340000.98);
+INSERT INTO User_Account(account_id, first_name, last_name, user_id,pass,addres,email,mobile, balance) 
+VALUES(3,'Juhi','Mathur', 'juhiM', 'aqd12@vf&', 'Sarjapura Gurugram', 'juhimathur@bing.com', '9988776655', 1000000.12);
+INSERT INTO User_Account(account_id, first_name, last_name, user_id,pass,addres,email,mobile, balance) 
+VALUES(4,'Shriya','Lahariya', 'shriyaL', 'abg$rrdd','Bellandur Kolkata','shriyalahariya@yahoo.com', '9090887651', 1220000.45);
+INSERT INTO User_Account(account_id, first_name, last_name, user_id,pass,addres,email,mobile, balance) 
+VALUES(5,'Sarika','Bali', 'sarikaB','oilk@vf&','HSR Jaipur', 'sarikabali@gmail.com', '8080909000', 1800002.90);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,11 @@
-CREATE TABLE Account(account_id INT PRIMARY KEY, first_name VARCHAR(255),last_name VARCHAR(255)
-);
+DROP TABLE IF EXISTS User_Account;
+CREATE TABLE User_Account(
+	account_id BIGINT NOT NULL PRIMARY KEY,
+ 	first_name VARCHAR(255), 
+ 	last_name VARCHAR(255), 
+ 	user_id VARCHAR(20) NOT NULL UNIQUE, 
+  	pass VARCHAR(10) NOT NULL,
+  	addres VARCHAR(255), 
+  	email VARCHAR(255), 
+  	mobile VARCHAR(10),
+ 	balance NUMERIC(10,3) NOT NULL);
