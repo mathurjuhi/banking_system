d2393ff92164c711f8d8c0ce4c0acc2207d4084a
msjava-proj <shreyalahariya@gmail.com>
2024-10-26 15:38:05 +0530

    created

--- src/main/java/com/bank/banking/controller/AccountController.java
+++ src/main/java/com/bank/banking/controller/AccountController.java
@@ -1,8 +1,11 @@
 package com.bank.banking.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bank.banking.model.UserAccount;
 
 @RestController
 @RequestMapping("/banking")
@@ -10,8 +13,9 @@ public class AccountController {
 	
 	@GetMapping
 	@RequestMapping("/test")
-	public String test() {
-		return "test";
+	public ResponseEntity<UserAccount> test() {
+		UserAccount user = new UserAccount(20006 , "Richa","Ranjan", "richaR2", "csf12cdzxa", "HAL Bangalore","richaranjan@yahoo.com", "8877664411", 8022000.00);
+		return ResponseEntity.ok(user);
 	}
 
 }
